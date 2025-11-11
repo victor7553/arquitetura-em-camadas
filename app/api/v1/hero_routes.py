4afd4c18f32b6cdd294deb50fef44011ae1dc3bb
victor7553 <victorhugo7553@gmail.com>
2025-11-10 22:24:54 -0300

    EN: refactor: enhance hero routes with additional CRUD operations and error handling. PT: refatorar: aprimorar as rotas dos heróis com operações CRUD adicionais e tratamento de erros.

--- app/api/v1/hero_routes.py
+++ app/api/v1/hero_routes.py
@@ -1,8 +1,11 @@
 # app/api/v1/hero_routes.py
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from app.crud.hero_crud import create_hero, delete_hero, get_hero_by_id, update_hero
+from app.schemas.hero_schema import HeroCreate, HeroUpdate
 from app.core.database import get_session
-from app.models.hero import Hero  # supondo que você tenha o modelo Hero
+from app.models.hero import Hero
+from app.services.hero_service import delete_hero_service  # supondo que você tenha o modelo Hero
 
 router = APIRouter()
 
@@ -10,3 +13,36 @@ router = APIRouter()
 def list_heroes(session: Session = Depends(get_session)):
     heroes = session.exec(select(Hero)).all()
     return heroes
+
+@router.get("/heroes/{hero_id}", response_model=Hero)
+def read_hero(hero_id: int, session: Session = Depends(get_session)):
+    hero = get_hero_by_id(session, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+@router.post("/create_hero", response_model=Hero)
+def create_hero_v2(hero: HeroCreate, session: Session = Depends(get_session)):
+    hero_db = Hero(**hero.model_dump())  # conversão
+    db_record = create_hero(session, hero_db)
+    return db_record
+
+@router.put("heroe_update/{hero_id}", response_model=Hero)
+def heroe_update(hero_id: int, hero: HeroUpdate, session: Session = Depends(get_session)):
+    db_record = update_hero(session, hero_id, hero)
+    if not db_record:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return db_record
+
+@router.delete("/heroes/{hero_id}", response_model=Hero)
+def hero_delete(hero_id: int, session: Session = Depends(get_session)):
+    db_record = delete_hero(session, hero_id)
+    if not db_record:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return db_record
+
+#  modo com serviço e regras de negócio
+# Lida com requisições HTTP e respostas
+@router.delete("/heroes_v2/{hero_id}")
+def delete_hero_v2(hero_id: int, session: Session = Depends(get_session)):
+    return delete_hero_service(session, hero_id)
