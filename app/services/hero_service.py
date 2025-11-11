801295df372c962e788654ed52d6d2c75ad6757d
victor7553 <victorhugo7553@gmail.com>
2025-11-10 22:27:31 -0300

    EN: refactor: add delete hero service with age restriction and error handling. PT: refatorar: adicionar serviço de exclusão de herói com restrição de idade e tratamento de erros.

--- app/services/hero_service.py
+++ app/services/hero_service.py
@@ -1,8 +1,21 @@
+from fastapi import HTTPException
 from sqlmodel import Session
-from app.crud.hero_crud import create_hero
+from app.crud.hero_crud import create_hero, delete_hero, get_hero_by_id
 from app.schemas.hero_schema import HeroCreate
 
 def register_hero(session: Session, data: HeroCreate):
     if data.age and data.age < 18:
         raise ValueError("Heróis menores de idade não são permitidos.")
     return create_hero(session, data)
+
+def delete_hero_service(session: Session, hero_id: int):
+    hero = get_hero_by_id(session, hero_id) # chama a função CRUD para obter o herói
+    if not hero:
+        raise HTTPException(status_code=404, detail="Herói não encontrado")
+
+    # Exemplo de regra de negócio
+    if hero.age is not None and hero.age < 18:
+        raise HTTPException(status_code=403, detail="Não é possível eliminar heróis menores de idade.")
+
+    delete_hero(session, hero_id) # chama a função CRUD para eliminar o herói
+    return {"message": f"Hero {hero.name} eliminado com sucesso"}
